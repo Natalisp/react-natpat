2ccb63e0128a0ac0e3258a97b82585a938817602
Natalisp <nathalipat@gmail.com>
2017-03-06 13:09:10 -0500

    update greeting style

--- src/components/word.js
+++ src/components/word.js
@@ -5,7 +5,7 @@ class Word extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      greeting: "Hello,",
+      greeting: "HELLO,",
       i: 0
     }
   }
@@ -22,20 +22,20 @@ class Word extends Component {
   }
 
   changeGreeting() {
-    var greetings = ["", "Privet,", "Hola,", "Ciao,", "Aloha,"]
+    var greetings = ["", "PRIVET,", "HOLA,", "CIAO,", "ALOHA,"]
     var newCount = this.state.i + 1;
     if(newCount < greetings.length) {
       this.setState({ greeting: greetings[newCount], i: newCount })
     } else {
       clearInterval(this.state.intervalId)
-      this.setState({ greeting: "Hello,", i: 0 })
+      this.setState({ greeting: "HELLO,", i: 0 })
     }
   }
 
   render() {
     return(
       <div>
-        <h1>{this.state.greeting}</h1>
+        {this.state.greeting}
         </div>
     )
   }
